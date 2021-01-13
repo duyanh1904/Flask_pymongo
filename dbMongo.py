974cf9c288f9535474e9fe2be0771cabe5d8db14
DUYANH <duyanh3897@gmail.com>
2021-01-13 19:53:56 +0700

    create project

--- dbMongo.py
+++ dbMongo.py
@@ -5,7 +5,7 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 db = myclient['TodoList']
 mycol = db['test']
 
-post = {"_id": 1, "name": "My first blog post!", "describe": "Lazada"}
+
 
 
 
--- validate.py
+++
@@ -1,7 +0,0 @@
-
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-
-class GreetUserForm(FlaskForm):
-    itemName = StringField(label=('Enter Your Name:'))
-    submit = SubmitField(label=('Submit'))
